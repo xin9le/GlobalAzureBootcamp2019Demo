8a10957f57d3586ba8c3e6934f9a0cf8d7a532e1
Takaaki Suzuki <xin9le@live.jp>
2019-04-23 20:17:31 +0900

    Unity プロジェクトのサンプルを追加

---
+++ src/Xtuber.Unity/Assets/Scripts/SignalRBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using UnityEngine;
+
+
+
+namespace Xtuber.Unity
+{
+    public class SignalRBehaviour : MonoBehaviour
+    {
+        private const string RootUrl = "http://xtuber-api.azurewebsites.net";
+        private HubConnection Connection { get; set; }
+
+
+        private async void Start()
+        {
+            this.Connection
+                = new HubConnectionBuilder()
+                .WithUrl($"{RootUrl}/api")
+                .Build();
+
+            this.Connection.On<string>("Receive", data =>
+            {
+                Debug.Log(data);
+            });
+
+            try
+            {
+                await this.Connection.StartAsync();
+                Debug.Log("Connected!");
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+            }
+        }
+    }
+}
